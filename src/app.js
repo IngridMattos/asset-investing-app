f8c62b3f07095c7b304cf522c323bee3590f9184
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-16 14:14:07 -0300

    chore::wrench:Configuração de ambiente

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(routes);
+app.use(routes);
 app.listen(process.env.EXPRESS_PORT, () => console.log('ouvindo porta', process.env.EXPRESS_PORT));
