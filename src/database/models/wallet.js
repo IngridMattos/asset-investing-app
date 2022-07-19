1b6932fe15b8713d9c5029e4cd53e94314244245
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-19 19:31:18 -0300

    style: :ok_hand:Alterando código para padrão novo do ESlint

--- src/database/models/wallet.js
+++ src/database/models/wallet.js
@@ -1,39 +1,42 @@
 const WalletShema = (sequelize, DataTypes) => {
-  const WalletTable = sequelize.define('Wallet', {
+  const WalletTable = sequelize.define(
+    'Wallet',
+    {
       codCliente: {
-        primaryKey:true,
+        primaryKey: true,
         foreignKey: true,
         type: DataTypes.INTEGER,
       },
       codAtivo: {
-        primaryKey:true,
+        primaryKey: true,
         foreignKey: true,
         type: DataTypes.INTEGER,
       },
       qtdeAtivo: {
         type: DataTypes.INTEGER,
-      }
-  },
-  {
-      timestamps: false
-    });
+      },
+    },
+    {
+      timestamps: false,
+    },
+  );
 
   WalletTable.associate = (models) => {
-      models.Client.belongsToMany(models.Asset, {
-          as: 'Assets',
-          through: 'Wallet',
-          foreignKey: 'codAtivo',
-          otherKey: 'codCliente'
-      });
-      models.Asset.belongsToMany(models.Client, {
-          as: 'categories',
-          through: 'Wallet',
-          foreignKey: 'codCliente',
-          otherKey: 'codAtivo'
-      });
-  }
+    models.Client.belongsToMany(models.Asset, {
+      as: 'Assets',
+      through: 'Wallet',
+      foreignKey: 'codAtivo',
+      otherKey: 'codCliente',
+    });
+    models.Asset.belongsToMany(models.Client, {
+      as: 'categories',
+      through: 'Wallet',
+      foreignKey: 'codCliente',
+      otherKey: 'codAtivo',
+    });
+  };
 
-return WalletTable;
+  return WalletTable;
 };
 
 module.exports = WalletShema;
