9045d6cd8b4cc96be69e4db61945572eb4363a37
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-19 13:47:00 -0300

    feat: :sparkles: implementando função de atualizar a quantidade de ativos disponiveis na corretora

--- src/services/buyAssetService.js
+++ src/services/buyAssetService.js
@@ -2,3 +2,12 @@ const model = require('../database/models');
 
 const { getAssets } = require('../utils/getAssets.js')
 const { getClientAssets } = require('../utils/getClientAssets.js')
+
+async function reduceAmountOfAssets(codAtivo, qtdeAtivoComprado, amountAssets) {
+
+    const [qtdUpdated] = await model.Asset.update({
+        qtdeAtivo: amountAssets - qtdeAtivoComprado
+    },
+    { where: { codAtivo } });
+    return qtdUpdated;
+};
