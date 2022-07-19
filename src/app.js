da514b2ce9116a64ce759399696a7dd05aa7547d
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-19 13:22:17 -0300

    feat: :sparkles: importando rota do arquivo routes para o uso do app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 
+const routes = require('./routes/routes.js');
+
 app.use(routes);
+
 app.listen(process.env.EXPRESS_PORT, () => console.log('ouvindo porta', process.env.EXPRESS_PORT));
