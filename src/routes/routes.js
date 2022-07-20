69d1b23a937f2b79c6d6415a59cfa08c4d4d5d37
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-20 17:29:04 -0300

    feat: :sparkles: Implementando e importando a função getAssetByIdController no arquivo de routes com o caminho investimentos/ativos/:codAtivo

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 const { buyAssetsController } = require('../controllers/buyAssetsController');
 const { sellAssetsController } = require('../controllers/sellAssetsController');
-const { getAllAssetsByIdController } = require('../controllers/getAllAssetsByIdController');
-const { getAssetsByIdController } = require('../controllers/getAssetsByIdController');
+const { getAllAssetsByIdClientController } = require('../controllers/getAllAssetsByIdClientController');
+const { getAssetByIdController } = require('../controllers/getAssetByIdController');
 
 router.post('/investimentos/comprar', buyAssetsController);
 router.post('/investimentos/vender', sellAssetsController);
-router.get('/ativos/:codCliente', getAllAssetsByIdController);
-router.get('/ativos/:codAtivo', getAssetsByIdController);
+router.get('/ativos/:codCliente', getAllAssetsByIdClientController);
+router.get('/investimentos/ativos/:codAtivo/', getAssetByIdController);
 
 module.exports = router;
