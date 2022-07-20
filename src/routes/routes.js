85aebb5033cffa1a132879ade670279c33aad4a4
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-20 13:38:11 -0300

    feat: :sparkles: Implementando rota /ativos/:codCliente no arquivo de routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const { buyAssetsController } = require('../controllers/buyAssetsController');
 const { sellAssetsController } = require('../controllers/sellAssetsController');
+const { getAllAssetsByIdController } = require('../controllers/getAllAssetsByIdController');
 
 router.post('/investimentos/comprar', buyAssetsController);
 router.post('/investimentos/vender', sellAssetsController);
+router.get('/ativos/:codCliente', getAllAssetsByIdController);
 
 module.exports = router;
