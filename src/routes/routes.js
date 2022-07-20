5c436afa754c36d87de9b0feef094115a58bcce8
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-20 16:11:21 -0300

    feat: :sparkles: Importando função do controller para a rota de /ativos/:codAtivo no arquivo routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const { buyAssetsController } = require('../controllers/buyAssetsController');
 const { sellAssetsController } = require('../controllers/sellAssetsController');
 const { getAllAssetsByIdController } = require('../controllers/getAllAssetsByIdController');
+const { getAssetsByIdController } = require('../controllers/getAssetsByIdController');
 
 router.post('/investimentos/comprar', buyAssetsController);
 router.post('/investimentos/vender', sellAssetsController);
 router.get('/ativos/:codCliente', getAllAssetsByIdController);
+router.get('/ativos/:codAtivo', getAssetsByIdController);
 
 module.exports = router;
