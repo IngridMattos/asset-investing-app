7d2eeae24cbbd01015d5fdf86901e0238040932e
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-20 20:50:34 -0300

    feat: :sparkles: importando rota da função que realiza o depósito e a validação do valor no arquivo routes para a realização do caminho /conta/deposito

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+const depositValidation = require('../middlewares/depositValidation');
 const { buyAssetsController } = require('../controllers/buyAssetsController');
 const { sellAssetsController } = require('../controllers/sellAssetsController');
 const { getAllAssetsByIdClientController } = require('../controllers/getAllAssetsByIdClientController');
 const { getAssetByIdController } = require('../controllers/getAssetByIdController');
+const { bankDepositClientController } = require('../controllers/bankDepositClientController');
 
 router.post('/investimentos/comprar', buyAssetsController);
 router.post('/investimentos/vender', sellAssetsController);
 router.get('/ativos/:codCliente', getAllAssetsByIdClientController);
 router.get('/investimentos/ativos/:codAtivo/', getAssetByIdController);
+router.post('/conta/deposito', depositValidation, bankDepositClientController);
 
 module.exports = router;
