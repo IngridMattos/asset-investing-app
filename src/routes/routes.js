bffc119d2ae01151fa99b7f9e3fa04a8b7cc12c9
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-20 20:58:04 -0300

    docs: :bulb: Comentários explicativos sobre mudança do padrão da rota ativos/id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+// ---- middleware ----
 
 const depositValidation = require('../middlewares/depositValidation');
+
+// ---- importações do controller -----
 const { buyAssetsController } = require('../controllers/buyAssetsController');
 const { sellAssetsController } = require('../controllers/sellAssetsController');
 const { getAllAssetsByIdClientController } = require('../controllers/getAllAssetsByIdClientController');
@@ -12,6 +15,10 @@ const { bankDepositClientController } = require('../controllers/bankDepositClien
 router.post('/investimentos/comprar', buyAssetsController);
 router.post('/investimentos/vender', sellAssetsController);
 router.get('/ativos/:codCliente', getAllAssetsByIdClientController);
+
+// Acrescentando o caminho /investimentos/ para a rota: /ativos/:codAtivo/  para que
+// assim exista distinção entre o /ativos/:codCliente na leitura e execução das rotas
+
 router.get('/investimentos/ativos/:codAtivo/', getAssetByIdController);
 router.post('/conta/deposito', depositValidation, bankDepositClientController);
 
