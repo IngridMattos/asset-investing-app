0313d3a99ac0678384047fbde193cd1b04823332
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-21 11:42:57 -0300

    feat: :sparkles: Criando função updateBalanceClient de atualização do valor do saque para a conta do cliente

--- src/services/bankDraftClientService.js
+++ src/services/bankDraftClientService.js
@@ -1,2 +1,13 @@
 const model = require('../database/models');
 const { getBalanceClient } = require('../utils/getBalanceClient');
+
+async function updateBalanceClient(codCliente, valor, amountBalanceClient) {
+  const [qtdUpdated] = await model.Client.update(
+    {
+      saldo: amountBalanceClient - Number(valor),
+    },
+    { where: { codCliente } },
+  );
+  return qtdUpdated;
+}
+
