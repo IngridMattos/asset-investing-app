394b5829fb491eb6a5db126e8b188097b3c85822
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-21 11:44:17 -0300

    feat: :sparkles: Criando função de resposta para o controle banckDraftClientService que vai executar a função de atualização da conta

--- src/services/bankDraftClientService.js
+++ src/services/bankDraftClientService.js
@@ -11,3 +11,27 @@ async function updateBalanceClient(codCliente, valor, amountBalanceClient) {
   return qtdUpdated;
 }
 
+async function bankDraftClientService({ codCliente, valor }) {
+  const amountBalanceClient = await getBalanceClient(codCliente);
+  if (valor > amountBalanceClient) {
+    return {
+      status: 400,
+      message: 'Valor na conta é insuficiente',
+    };
+  }
+  const qtdUpdated = await updateBalanceClient(codCliente, valor, amountBalanceClient);
+  if (qtdUpdated !== 1) {
+    return {
+      status: 400,
+      message: 'Algo deu errado, o saque não pode ser realizado',
+    };
+  }
+  return {
+    status: 200,
+    message: 'Saque realizado com sucesso',
+  };
+}
+
+module.exports = {
+  bankDraftClientService,
+};
