9086cf68ddc29948c350d3cfb67b2e12baa2ad08
Ingrid Mattos <ingrid_matt@hotmail.com>
2022-07-21 11:31:51 -0300

    refactor: :recycle: Alterando resposta do middleware de validação do valor de deposito para ser util para o saque

--- src/middlewares/depositValidation.js
+++ src/middlewares/depositValidation.js
@@ -2,7 +2,7 @@ module.exports = async (req, res, next) => {
   const { valor } = req.body;
   if (Number(valor) <= 0) {
     return res.status(400)
-      .send({ message: 'O Valor do depósito não pode ser negativo ou nulo' });
+      .send({ message: 'O Valor não pode ser negativo ou nulo' });
   }
 
   next();
